378a7c6acc3ce9b813e423641de996d0aa4ff35d
sunjinjie <592513724@qq.com>
2019-06-26 17:21:49 +0800

    工程介绍

--- src/pages/productIntro/component/developer.vue
+++ src/pages/productIntro/component/developer.vue
@@ -10,7 +10,7 @@ export default {
     data() {
         return {
           developerName: 's进j',
-          intro: '参照网上一些大神的界面，代码才写出的这个前端的项目,用到了 vue(框架) 、 Element-UI(组件) 、vue-router(动态路由加载nav) 、 vuex(权限控制) 、axios(数据请求)等'
+          intro: '参照网上一些大神的界面，代码才写出的这个前端的项目,用到了 vue(框架) 、 Element-UI(组件，界面) 、vue-router(动态路由加载nav，鉴权) 、 vuex(状态管理) 、axios(数据请求)等'
         }
     }
 }
